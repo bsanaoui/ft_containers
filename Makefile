d0d5a9ec40ff65a33e695b29b86f5d7553dc559b
Brahim Sanaoui <bsanaoui@e1r4p1.1337.ma>
2021-12-20 20:57:58 +0100

    Setup env

--- Makefile
+++ Makefile
@@ -1,3 +1,22 @@
+NAME = Containers
+
+CC = clang++
+FLAGS = -Werror -Wextra -Wall
+
+SRC = test.cpp
+
+all : $(NAME)
+
+$(NAME) : $(SRC)
+	@$(CC) $(SRC) $(FLAG) -o $(NAME)
+
+fclean : clean
+	@rm -f $(NAME)
+
+clean :
+
+re : fclean all
+
 push : 
 	git add .
 	git commit -m "$(m)"
---
+++ vector.hpp
@@ -0,0 +1,23 @@
+#ifndef     __VECTOR__HPP__
+#define     __VECTOE__HPP__
+
+#include <iostream>
+
+// ==================================== Class template Vector ============================================ //
+
+// -------- template < Create Namespace "ft" ------------ //
+namespace ft
+{
+    // *********************************** //
+    template < class T, class Alloc = std::allocator<T> >
+    
+    // ************************************ //
+    // *** Vector Template *** //
+    // *********************************** //   
+    class vector
+    {
+        
+    };
+} // namespace ft
+
+#endif
